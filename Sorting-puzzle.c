f7c83a791aab29bdfc0b4ce03a966667c73cd084
Arjun <arjunraju122001@gmail.com>
2024-09-15 15:14:37 +0530

    Created sorting game without winner announcement

---
+++ Sorting-puzzle.c
@@ -0,0 +1,126 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define ROW 4
+#define COL 4
+
+int checkRepeat(int val, int mat[ROW][COL]){
+    
+    if(val == 0){
+        return 1;
+    }
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {
+            if(mat[i][j] == val){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void displayMatrix(int mat[ROW][COL]){
+    
+    printf("The game is: \n");
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
+void setMatrix(int mat[ROW][COL]){
+    
+    srand(time(NULL));
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {   
+            int val;
+            if(i == ROW-1 && j == COL -1){
+                break;
+            }
+            do{
+                val = rand() % (ROW * COL);
+            }while(checkRepeat(val, mat));
+            mat[i][j] = val;
+        }
+        
+    }
+    mat[ROW -1][COL -1] = 0; 
+    
+}
+
+void swap(int matrix[ROW][COL], int m, int n, int p, int q){
+
+    // if(p < 0 || q < 0 || p > (ROW-1) || q > (COL-1)){
+    //     printf("Invalid move..........\n");
+    // }else{
+
+        matrix[m][n] = matrix[p][q];
+        matrix[p][q] = 0;
+    // }
+
+}
+
+char menu(){
+    
+    printf("Move 0 using W,S,A,D and press X to quit\n");
+    return getch();
+}
+
+void main(){
+    
+    printf("Welcome to the game..\n");
+    int matrix[ROW][COL];
+
+    setMatrix(matrix);
+    displayMatrix(matrix);
+    
+    int m = ROW-1, n = COL-1;
+    char choice;
+
+    for ( choice = menu(); choice != 'x'; choice = menu())
+    {
+        switch (choice)
+        {
+            case 'w':
+                if(m-1 >= 0){
+                    swap(matrix, m, n, m-1, n);
+                    m--;
+                }
+                break;
+            case 's':
+                if(m+1 < ROW){
+                    swap(matrix, m, n, m+1, n);
+                    m++;
+                }
+                break;
+            case 'a':
+                if(n-1 >= 0){
+                    swap(matrix, m, n, m, n-1);
+                    n--;
+                }                
+                break;
+            case 'd':
+                if(n+1 < COL){
+                    swap(matrix, m, n, m, n+1);
+                    n++;
+                }
+                break;
+            default:
+                printf("Wrong choice\n");
+                break;
+        }
+        system("cls");
+        displayMatrix(matrix);
+    }
+    
+
+}
